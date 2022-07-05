53101702c1758dc44b913242e22263b54796bc5e
Mohamed abdelhakeem <108456376+duxpixels@users.noreply.github.com>
2022-07-05 13:06:25 +0300

    Make it public

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./componets/Header";
+import Home from "./componets/Home";
+
+
+export default function App(){
+
+    return(
+        <div className="">
+            <Header />
+            <Home darkMode={false} />
+        </div>
+    )
+}
+
+
+
---
+++ src/Boxes_data.js
@@ -0,0 +1,10 @@
+let boxes = []
+
+for(let i=0;i<1500;i++){
+    boxes.push({
+        id:i,
+        on:true,
+    })
+}
+
+export default boxes;
